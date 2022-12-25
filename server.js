e2c4c950ddea4fa372543f7d7a5651073782c3db
knight392 <61463986+knight392@users.noreply.github.com>
2022-12-25 10:11:39 +0000

    feat: feat add pwdCheck

--- server.js
+++ server.js
@@ -2,5 +2,19 @@ const express = require('express');
 const app = new express();//创建express实例
 
 app.use(express.static('static'));
+
+app.get('/check_pwd', (req, res) => {
+    const pwd = req.query.pwd;
+    res.json({
+        res: pwd === '老男人的小仙女'
+    })
+});
+
+app.get('/img_list', (req, res) => {
+    res.json([
+        'skfjlksfj',
+        'slkfjlsfj'
+    ])
+});
 app.listen(9001);
 console.log('listen on port 9001');
--- static/carousel.js
+++ static/carousel.js
@@ -17,7 +17,7 @@ NOTE:
 */
 
 // ===================== start =======================
-setTimeout(init, 100);
+
 
 let odrag = document.getElementById("drag-container");
 let ospin = document.getElementById("spin-container");
@@ -34,7 +34,55 @@ let ground = document.getElementById("ground");
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
 
-function init(delayTime) {
+
+function _removePointEvent() {
+  // setup events
+  document.onpointerdown = function (e) {
+    clearInterval(odrag.timer);
+    e = e || window.event;
+    let sX = e.clientX,
+      sY = e.clientY;
+
+    this.onpointermove = function (e) {
+      e = e || window.event;
+      let nX = e.clientX,
+        nY = e.clientY;
+      desX = nX - sX;
+      desY = nY - sY;
+      tX += desX * 0.1;
+      tY += desY * 0.1;
+      applyTranform(odrag);
+      sX = nX;
+      sY = nY;
+    };
+
+    this.onpointerup = function (e) {
+      odrag.timer = setInterval(function () {
+        desX *= 0.95;
+        desY *= 0.95;
+        tX += desX * 0.1;
+        tY += desY * 0.1;
+        applyTranform(odrag);
+        playSpin(false);
+        if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+          clearInterval(odrag.timer);
+          playSpin(true);
+        }
+      }, 17);
+      this.onpointermove = this.onpointerup = null;
+    };
+    return false;
+  };
+
+  document.onmousewheel = function (e) {
+    e = e || window.event;
+    let d = e.wheelDelta / 20 || -e.detail;
+    radius += d;
+    _init(1);
+  };
+}
+
+function _initCarousel(delayTime) {
   for (let i = 0; i < aEle.length; i++) {
     aEle[i].style.transform =
       "rotateY(" +
@@ -52,7 +100,6 @@ function applyTranform(obj) {
   // Constrain the angle of camera (between 0 and 180)
   if (tY > 180) tY = 180;
   if (tY < 0) tY = 0;
-
   // Apply the angle
   obj.style.transform = "rotateX(" + -tY + "deg) rotateY(" + tX + "deg)";
 }
@@ -78,48 +125,3 @@ if (autoRotate) {
   )}s infinite linear`;
 }
 
-// setup events
-document.onpointerdown = function (e) {
-  clearInterval(odrag.timer);
-  e = e || window.event;
-  let sX = e.clientX,
-    sY = e.clientY;
-
-  this.onpointermove = function (e) {
-    e = e || window.event;
-    let nX = e.clientX,
-      nY = e.clientY;
-    desX = nX - sX;
-    desY = nY - sY;
-    tX += desX * 0.1;
-    tY += desY * 0.1;
-    applyTranform(odrag);
-    sX = nX;
-    sY = nY;
-  };
-
-  this.onpointerup = function (e) {
-    odrag.timer = setInterval(function () {
-      desX *= 0.95;
-      desY *= 0.95;
-      tX += desX * 0.1;
-      tY += desY * 0.1;
-      applyTranform(odrag);
-      playSpin(false);
-      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
-        clearInterval(odrag.timer);
-        playSpin(true);
-      }
-    }, 17);
-    this.onpointermove = this.onpointerup = null;
-  };
-
-  return false;
-};
-
-document.onmousewheel = function (e) {
-  e = e || window.event;
-  let d = e.wheelDelta / 20 || -e.detail;
-  radius += d;
-  init(1);
-};
